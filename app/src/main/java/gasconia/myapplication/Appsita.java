afba9fd6e008721e80011de90e54c9daf6589f4e
pablo-psichalis <pablo.psrz@gmail.com>
2016-08-20 19:27:40 +0200

    Experimentasion en la ramita.

---
+++ app/src/main/java/gasconia/myapplication/Appsita.java
@@ -0,0 +1,13 @@
+package gasconia.myapplication;
+
+/**
+ * Created by Pablo on 20/08/2016.
+ */
+public class Appsita {
+
+    private String cod_app;
+
+    Appsita(){
+        cod_app = "XD333";
+    }
+}
